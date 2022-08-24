1a57922cda6e60f66f5bbcc4615c399167df33dc
João Victor Lira Victor <109807072+eujoaovlira@users.noreply.github.com>
2022-08-24 14:01:49 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,11 +1,51 @@
-class LadyGaga :
-  def __init__(self, LadyGaga):
-    self.LadyGaga = LadyGaga
+import java.io.Serializable;
 
+  public class Pessoa implements Serializable {
+    private String nome;
+    private double pc;   // peso corporal
+    private double alt;  // altura em metros
 
-  def exibir1(self):
-    print(self.LadyGaga)
+    public Pessoa(String nome, double pc, double alt) {
+      this.nome = nome;
+      this.pc = pc;
+      this.alt = alt;
+    }
 
+    public String getNome() {
+      return nome;
+    }
 
-fla = LadyGaga('LadyGaga ganha mas um grammy !!')
-fla.exibir1()
+    public void setNome(String nome) {
+      this.nome = nome;
+    }
+
+    public double getPC() {
+      return pc;
+    }
+
+    public void setPC(float pc) {
+      this.pc = pc;
+    }
+
+    public double getAlt() {
+      return alt;
+    }
+
+    public void setAlt(float alt) {
+      this.alt = alt;
+    }
+
+    public double IMC() {
+      return(getPC() / (getAlt() * getAlt()));
+    }
+
+    public String interpretaIMC() {
+      double vlrIMC = IMC();
+      if (vlrIMC < 18.5)
+         return("baixo peso");
+      else if (vlrIMC < 25.0)
+              return("peso adequado");
+           else if (vlrIMC < 30.0)
+                   return("sobrepeso");
+                else return("obesidade");
+    }
