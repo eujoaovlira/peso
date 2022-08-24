c702a03148288bce7ded8a06099e0d1906e9e07f
João Victor Lira Victor <109807072+eujoaovlira@users.noreply.github.com>
2022-08-24 13:09:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+Definição informal: grafo (graph) é um conjunto de vértices (ou nodos), interconectados dois a dois por
+arestas (ou arcos)
+
+Em programação, o conceito de grafos é muito utilizado em matriz, auxiliando a visualizar como fazer um 
+caminho mais rápido para chegar do vértice 1 ao 5, por exemplo, são os aplicativos de mapas.
+
+Exemplo de ligação simplex:
+
+Dados apenas para uma direção,
+  A->B
+
+Exemplo de ligação full duplex:
+
+É o modo de transmissão em sentido duplo ou bidirecional simultâneo.
+  A->B
+  A<-B
